1241b97766e7195dc1751432ebc999ff3a640a40
Vitor Vieira Machado <164572708+vitorvieira7@users.noreply.github.com>
2024-07-04 10:23:24 -0300

    commit

---
+++ exc_1/src/CalculadoraSimples.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o operador (+, -, *, /): ");
+        char operador = scanner.next().charAt(0);
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        double resultado;
+
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Operador inválido.");
+                return;
+        }
+
+        System.out.println("O resultado é: " + resultado);
+    }
+}
