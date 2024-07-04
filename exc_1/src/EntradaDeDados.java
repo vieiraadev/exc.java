981df7414784445edb8de7a7bf29b6d75d8f9f7c
Vitor Vieira Machado <164572708+vitorvieira7@users.noreply.github.com>
2024-07-04 10:27:35 -0300

    commit

--- exc_1/src/EntradaDeDados.java
+++ exc_1/src/EntradaDeDados.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 
 public class EntradaDeDados {
     public static void main(String[] args) {
-        // Cria uma instância do Scanner para ler a entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita ao usuário que insira um número
         System.out.print("Digite um número: ");
         int numero = scanner.nextInt();
 
-        // Exibe o número digitado pelo usuário
         System.out.println("Você digitou: " + numero);
 
-        // Fecha o Scanner
         scanner.close();
     }
 }
