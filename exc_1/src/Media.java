f2b1749c575214765eeb2f9192cf35bf4ab64ea8
Vitor Vieira Machado <164572708+vitorvieira7@users.noreply.github.com>
2024-07-04 10:34:05 -0300

    commit

---
+++ exc_1/src/Media.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de números: ");
+        int quantidade = scanner.nextInt();
+
+        double soma = 0;
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            double numero = scanner.nextDouble();
+            soma += numero;
+        }
+
+        double media = soma / quantidade;
+        System.out.println("A média é: " + media);
+    }
+}
